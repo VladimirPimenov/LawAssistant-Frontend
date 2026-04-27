17b7799cf7f5056ba2004c595150ff71367ba557
VladimirPimenov <pimenovv2000@gmail.com>
2026-04-28 01:48:45 +0300

    В список авторов добалена почта

--- src/components/AuthorsList/AuthorItem.jsx
+++ src/components/AuthorsList/AuthorItem.jsx
@@ -3,7 +3,11 @@ import "./AuthorItem.css"
 const AuthorItem = ({author, onRemoveAuthor}) => {
     return (
         <div className="author-row">
-            <div className="author-info">{author.firstName} {author.lastName}</div>
+            <div className="author-info">
+                <div className="author-name">{author.firstName} {author.lastName}</div>
+                <div className="author-email">{author.email}</div>
+            </div>
+
             <button type="button" onClick={() => onRemoveAuthor(author)}>Удалить</button>
         </div>
     )
